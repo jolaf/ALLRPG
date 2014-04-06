560c9b490dba1fd248511ded2d40d71e81515411
Leonid Tsarev <leo@bastilia.ru>
2014-04-06 20:18:41 +0400

    Фиксы statusboard

--- site2/public_html/kind_gamereport.php
+++ site2/public_html/kind_gamereport.php
@@ -5,15 +5,15 @@ if($_SESSION["user_id"]!='' /*&& $workrights["site"]["report"]*/) {
   $site_id = intval($_SESSION['siteid']);
 	
 	$result = mysql_query ("
-    SELECT rl.id, rl.parent, rl.name, COUNT(r.id) AS total_count, SUM(IF(r.status=1, 1, 0)) AS podana, SUM(IF(r.status=2, 1, 0)) AS inprocess, SUM(IF(r.status=3, 1, 0)) AS accepted, SUM(IF (r.alltold = 1, 0, 1)) AS alltold, SUM(IF (r.moneydone = 1, 1,0)) AS moneydone
+    SELECT rl.id, rl.parent, rl.name, COUNT(r.id) AS total_count, SUM(IF(r.status=1, 1, 0)) AS podana, SUM(IF(r.status=2, 1, 0)) AS inprocess, SUM(IF(r.status=3, 1, 0)) AS accepted, SUM(IF (r.alltold = 1, 0, 1)) AS alltold, SUM(IF (r.moneydone+0 = 1, 0,1)) AS moneydone
     FROM  (
       SELECT id, parent, name, code FROM {$prefix}roleslocat WHERE site_id = $site_id
       UNION ALL 
       SELECT 0,0, 'Итого', 0
     ) rl
-    LEFT JOIN {$prefix}rolevacancy rv ON rv.locat = rl.id
+    LEFT JOIN {$prefix}rolevacancy rv ON rv.locat = rl.id 
     LEFT JOIN {$prefix}roles r ON r.vacancy = rv.id
-    WHERE r.id IS NULL OR (r.site_id = $site_id AND r.todelete = 0 AND r.todelete2 = 0 AND r.status<>4)
+    WHERE (r.id IS NOT NULL OR rl.Id is NOT NULL) AND (r.id IS NULL OR (r.site_id = $site_id AND r.todelete = 0 AND r.todelete2 = 0 AND r.status<>4))
     GROUP BY rl.id, rl.parent, rl.name  ORDER BY rl.code ASC");
     
    
