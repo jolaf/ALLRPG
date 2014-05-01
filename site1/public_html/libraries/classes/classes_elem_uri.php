7e50232964f28cd119c2c53598be215e759d52a3
Leonid Tsarev <leo@bastilia.ru>
2014-05-01 22:42:25 +0400

    Новый тип поля — ссылка

---
+++ site1/public_html/libraries/classes/classes_elem_uri.php
@@ -0,0 +1,29 @@
+﻿<?php
+
+class netTextUri extends netTextarea {
+	function netTextUri($params) {
+		$this->netTextarea($params);
+		if ($params['rows'] > 1)
+		{
+      die("URL has to be one-line");
+		}
+	}
+
+	function draw($type, $can, $linenum) {
+		if($can=="write")
+		{
+			return $this->trueDraw($linenum);
+		}
+		else
+		{
+      $value = decodesafe(encode($this->getVal()));
+			return "<a href=\"$value\">$value</a>";
+		}
+	}
+
+	function destroy() {
+		unset($this);
+	}
+}
+
+?>
